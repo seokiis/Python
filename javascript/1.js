23f423cb61f98a7dce03a4b57700802e7ee606f8
kimseokhee <cat1181123@naver.com>
2024-07-01 17:48:18 +0900

    js basic

--- javascript/1.js
+++
@@ -1,5 +0,0 @@
-const modifiedArr=new_a.map(num=>{
-  if(num<0){
-    return Math.abs(num)%26;
-  }
-})
---
+++ javascript/4153.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const io = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const inputs = fs.readFileSync(io).toString().trim().split("\n");
+
+for (let line of inputs) {
+  const [a, b, c] = line.split(" ").map(Number);
+
+  if (a === 0 && b === 0 && c === 0) break;
+  const max = Math.max(a, b, c);
+  if (max === a) {
+    if (a ** 2 === b ** 2 + c ** 2) console.log("right");
+    else console.log("wrong");
+  } else if (max === b) {
+    if (b ** 2 === a ** 2 + c ** 2) console.log("right");
+    else console.log("wrong");
+  } else {
+    if (c ** 2 === a ** 2 + b ** 2) console.log("right");
+    else console.log("wrong");
+  }
+}
